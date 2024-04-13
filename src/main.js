ad4d2b58e447d2b7a45fb441d00a4afeac26dfbe
zzm <a15141668067@163.com>
2024-04-14 07:47:05 +0800

    fxxk

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
+// 加载全局样式
+import './styles/index.less'
+
+// 加载动态设置 REM 基准值
+import 'amfe-flexible'
+
+// 注册使用 Vant 组件库
+Vue.use(Vant)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 路由表
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
